ac92a46bd485f8cdfde0ab34cb96ad956a1bf34d
Volodymyr Ruban <Tulen2001@gmail.com>
2021-11-16 14:13:45 +0200

    Created class sudoku | Created class Constants

---
+++ Sudoku/Sudoku/Program.cs
@@ -0,0 +1,14 @@
+﻿	using System;
+	using System.Reflection.Metadata.Ecma335;
+
+	namespace Sudoku
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var sudoku = new Sudoku(9);
+			
+		}
+	}
+}
---
+++ Sudoku/Sudoku/Sudoku.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace Sudoku
+{
+	public class Sudoku
+	{
+		private readonly int _sudokuSize;
+		private readonly int _squareSize;
+		private int[,] _sudoku;
+
+		public Sudoku(int size)
+		{
+			
+				
+		}
+
+		private bool IsSizeValid(int size) => size > Constants.MIN_SIZE;
+		
+
+		public void FillSudokuWithRandomValues()
+		{
+			
+		}
+
+		public override string ToString()
+		{
+			return base.ToString();
+		}
+	}
+}
